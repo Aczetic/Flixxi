535e5986d9aa97aa3a9515f5f5e941a2c8f15929
Ananya <justdiscussanything@gmail.com>
2025-03-02 23:18:50 +0530

    removed futura

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function func(){
-    console.log(1);
-    return 0;
+function func() {
+  console.log(1);
+  return 0;
 }
 
-console.log('hello');
-console...log('m');
-console.log('mellow');
+console.log("hello");
+
+console.log("mellow");
